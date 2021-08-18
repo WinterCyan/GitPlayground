97269e52a521422af79171f8922a90d239de7247
Winter Cyan <wintercyan98@gmail.com>
2021-08-18 17:45:07 +0800

    some change on windows.

--- python_on_windows.py
+++ python_on_windows.py
@@ -2,5 +2,12 @@ def main():
     print('this is windows\' repository')
 
 
+# some changes.
+def some_method():
+    print('func ability.')
+    # TODO: solve some bug.
+    pass
+
+
 if __name__ == '__main__':
     main()
